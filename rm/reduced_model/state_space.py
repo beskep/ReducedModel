fix: numerical error (load matrix)

rm/reduced_model/state_space.py
@@ -25,6 +25,7 @@ def _nodes(matrix: Union[csc_matrix, StrPath],
     else:
       matrix = reader.read_matrix(path=matrix, square=False)
 
+  matrix[np.nonzero(matrix)] = 1.0  # 주의! target nodes의 원소는 1이어야 함.
   matrix /= matrix.data.size  # 0이 아닌 node 개수로 나눔. 왜인지는 모름...
 
   return matrix
@@ -34,9 +35,12 @@ def _state_space(A: csc_matrix, B: csc_matrix, J: csc_matrix):
   return StateSpace(A.toarray(), B.toarray(), J.toarray(), 0.0)
 
 
-def reduce_model(state_space: StateSpace, order: int) -> StateSpace:
+def reduce_model(state_space: StateSpace,
+                 order: int,
+                 method='matchdc') -> StateSpace:
+  # TODO 리덕션 방식 cli에서 옵션으로
   with utils.console.status('Reducing model'):
-    reduced: StateSpace = balred(sys=state_space, orders=order)
+    reduced: StateSpace = balred(sys=state_space, orders=order, method=method)
 
   logger.info(
       '모델 리덕션 완료 (dim {} to {})',

script/compute_validation_data.py
@@ -7,21 +7,20 @@ from rm.reduced_model.reduced_model import ModelReducer
 
 
 def compute(orders):
-  root_dir = Path(__file__).parents[1]
-  save_dir = root_dir.joinpath('res')
+  data_dir = Path(__file__).parents[1].joinpath('sample')
+  save_dir = data_dir
 
   for order in orders:
     model = ModelReducer(order=order)
 
-    model.read_matrices(damping=root_dir.joinpath('res/C.txt'),
-                        stiffness=root_dir.joinpath('res/K.txt'),
-                        internal_load=root_dir.joinpath('res/Lin.txt'),
-                        external_load=root_dir.joinpath('res/Lout.txt'))
+    model.read_matrices(damping=data_dir.joinpath('DMPV1.txt'),
+                        stiffness=data_dir.joinpath('STIF1.txt'),
+                        internal_load=data_dir.joinpath('LOAD1int.txt'),
+                        external_load=data_dir.joinpath('LOAD1ext.txt'))
 
     target_files = [
-        root_dir.joinpath('res/specific1.txt'),
-        root_dir.joinpath('res/specific2.txt'),
-        root_dir.joinpath('res/specific3.txt'),
+        data_dir.joinpath('target1.txt'),
+        data_dir.joinpath('target2.txt'),
     ]
     model.set_target_nodes(target_files)
 
@@ -34,10 +33,13 @@ def compute(orders):
                                     (10 + 5 * np.sin(np.pi * 0.5 * x / 6)),
                                     loc=Location.Exterior)
 
-    fname = save_dir.joinpath('reduction_output_{}.txt'.format(order))
-    model.compute(dt=3600, time_step=200, fname=fname)
+    model.order = order
+    model.reduce_model()
+    res = model.compute(dt=3600, time_step=200)
+    print(res)
 
 
 if __name__ == '__main__':
   orders = [None, 50, 20, 10, 5, 2, 1]
+  orders = [None]
   compute(orders=orders)